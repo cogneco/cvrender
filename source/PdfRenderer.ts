4b8345f298cc9d2a0fae505ad558245570b30441
Simon Mika <simon@mika.se>
2018-11-08 16:37:01 +0100

    Improved pdf-rendering.

--- source/PdfRenderer.ts
+++ source/PdfRenderer.ts
@@ -2,16 +2,15 @@ import { CV } from "./CV"
 import { addRenderer } from "./Renderer"
 import { HtmlRenderer } from "./HtmlRenderer";
 
-import * as inline from "inline-html"
 import * as fs from "fs"
 import * as cp from "child_process"
-import * as async from "async"
 
 export class PdfRenderer extends HtmlRenderer {
 	constructor() { super() }
 	async render(data: CV, output: fs.WriteStream, style?: string): Promise<boolean> {
-		return output.write(cp.execFileSync("prince", ["--javascript", "-", "-o", "-"], { input: this.renderString(data, style), cwd: style ? style.replace(/\/[a-z,A-Z,-,_,\.]+$/, "") : "" }))
+		const input = this.renderString(data, style)
+		const d = cp.execFileSync("prince", ["--javascript", "-", "-o", "-"], { input })
+		return output.write(d.join())
 	}
 }
 addRenderer("pdf", (data: CV, output: fs.WriteStream, style: string) => new PdfRenderer().render(data, output, style))
-
